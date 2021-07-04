2873095bc2278acab4dd8bfa2bfb18698103957f
mhreza76 <rezabaiust@gmail.com>
2021-07-05 02:18:34 +0600

    Number function and conversions

---
+++ numbers.js
@@ -0,0 +1,14 @@
+var n = 45
+var nn = 76.45
+var str = '85.6'
+console.log(Number(n))
+console.log(Number(nn))
+console.log(Number(str))
+console.log(Number.parseFloat(n))
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log('34' * 2)
+console.log('ABC' * 2)
+console.log(1 / 0)
