ea8c115a52a65bb3a9379d274ed98aabf18ffedf
mhreza76 <rezabaiust@gmail.com>
2021-07-05 02:50:37 +0600

    Number conversions

--- numbers.js
+++ numbers.js
@@ -12,3 +12,11 @@ console.log(Number.MIN_SAFE_INTEGER)
 console.log('34' * 2)
 console.log('ABC' * 2)
 console.log(1 / 0)
+
+// HexaDecimal number need to start with 0x or 0X
+var hex = 0xffff
+console.log(hex)
+
+// Octal number need to start with 0
+var oct = 0242
+console.log(oct)
